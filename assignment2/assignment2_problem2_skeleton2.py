98ee426429d50c49c23c03feb0d673a70771aee8
jmarkusson <jomarkusson@gmail.com>
2026-04-16 10:54:33 +0200

    The skeleten code for ass2

---
+++ assignment2/assignment2_problem2_skeleton2.py
@@ -0,0 +1,127 @@
+import os
+import argparse
+import sys
+import time
+import multiprocessing as mp
+
+def get_filenames(path):
+    """
+    A generator function: Iterates through all .txt files in the path and
+    returns the full names of the files
+
+    Parameters:
+    - path : string, path to walk through
+
+    Yields:
+    The full filenames of all files ending in .txt
+    """
+    for (root, dirs, files) in os.walk(path):
+        for file in files:
+            if file.endswith('.txt'):
+                yield f'{root}/{file}'
+
+def get_file(path):
+    """
+    Reads the content of the file and returns it as a string.
+
+    Parameters:
+    - path : string, path to a file
+
+    Return value:
+    The content of the file in a string.
+    """
+    with open(path,'r') as f:
+        return f.read()
+
+def count_words_in_file(filename_queue,wordcount_queue,batch_size):
+    """
+    Counts the number of occurrences of words in the file
+    Performs counting until a None is encountered in the queue
+    Counts are stored in wordcount_queue
+    Whitespace is ignored
+
+    Parameters:
+    - filename_queue, multiprocessing queue :  will contain filenames and None as a sentinel to indicate end of input
+    - wordcount_queue, multiprocessing queue : (word,count) dictionaries are put in the queue, and end of input is indicated with a None
+    - batch_size, int : size of batches to process
+
+    Returns: None
+    """
+    raise NotImplementedError
+
+
+
+def get_top10(counts):
+    """
+    Determines the 10 words with the most occurrences.
+    Ties can be solved arbitrarily.
+
+    Parameters:
+    - counts, dictionary : a mapping from words (str) to counts (int)
+    
+    Return value:
+    A list of (count,word) pairs (int,str)
+    """
+    raise NotImplementedError
+
+
+
+def merge_counts(out_queue,wordcount_queue,num_workers):
+    """
+    Merges the counts from the queue into the shared dict global_counts. 
+    Quits when num_workers Nones have been encountered.
+
+    Parameters:
+    - global_counts, manager dict : global dictionary where to store the counts
+    - wordcount_queue, manager queue : queue that contains (word,count) pairs and Nones to signal end of input from a worker
+    - num_workers, int : number of workers (i.e., how many Nones to expect)
+
+    Return value: None
+    """
+    raise NotImplementedError
+
+
+
+def compute_checksum(counts):
+    """
+    Computes the checksum for the counts as follows:
+    The checksum is the sum of products of the length of the word and its count
+
+    Parameters:
+    - counts, dictionary : word to count dictionary
+
+    Return value:
+    The checksum (int)
+    """
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Counts words of all the text files in the given directory')
+    parser.add_argument('-w', '--num-workers', help = 'Number of workers', default=1, type=int)
+    parser.add_argument('-b', '--batch-size', help = 'Batch size', default=1, type=int)
+    parser.add_argument('path', help = 'Path that contains text files')
+    args = parser.parse_args()
+
+    path = args.path
+
+    if not os.path.isdir(path):
+        sys.stderr.write(f'{sys.argv[0]}: ERROR: `{path}\' is not a valid directory!\n')
+        quit(1)
+
+    num_workers = args.num_workers
+    if num_workers < 1:
+        sys.stderr.write(f'{sys.argv[0]}: ERROR: Number of workers must be positive (got {num_workers})!\n')
+        quit(1)
+
+    batch_size = args.batch_size
+    if batch_size < 1:
+        sys.stderr.write(f'{sys.argv[0]}: ERROR: Batch size must be positive (got {batch_size})!\n')
+        quit(1)
+
+    # construct workers and queues
+    # construct a special merger process
+    # put filenames into the input queue
+    # workers then put dictionaries for the merger
+    # the merger shall return the checksum and top 10 through the out queue
+    
