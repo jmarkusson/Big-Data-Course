d59a4d08dcf81cb432d139f4657806a14438fdb1
Dannede <dangora@student.chalmers.se>
2026-04-27 14:00:28 +0200

    drawio fil tillagd

--- Assignment3/assignment3_problem1_skeleton.py
+++ Assignment3/assignment3_problem1_skeleton.py
@@ -3,7 +3,28 @@
 from mrjob.job import MRJob
 
 class MRMineral(MRJob):
-    pass # write your implementation here
+
+    def mapper(self, _, row):
+
+        cols = row.split(',')
+
+        star = cols[0]
+        constellation = cols[1]
+        mineral_value = float(cols[-1])
+
+        if star == "Prime":
+            star_system = constellation
+        else:
+            star_system = star + " " + constellation
+
+        yield star_system, mineral_value
+
+
+    def reducer(self, star_system, list_of_mineral_values):
+
+        total = sum(list_of_mineral_values)
+
+        yield star_system, total
 
 if __name__ == '__main__':
     MRMineral().run()
