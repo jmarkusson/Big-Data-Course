f20d9d5fa9a6866a0960ddbf7a1b0e35dbb51e80
jmarkusson <jomarkusson@gmail.com>
2026-05-11 12:48:07 +0200

    plotten färdig

--- assignment4/plot2c.py
+++ assignment4/plot2c.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 
 
 cores = [1,2,4,8,16,32,64]
-runtimes = [X, Y, 1103.2, 593.8, 323.7, 198.9, 125.9]
-speedup = [100.28154754638672 / runtime for runtime in runtimes]
+runtimes = [587.6, 584.4, 318.8, 172.3, 106.4, 66.7, 55.5]
+speedup = [587.6 / runtime for runtime in runtimes]
 
 plt.plot(cores, speedup, marker='o')
 plt.xlabel("Number of cores")
 plt.ylabel("Speedup")
-plt.title("Scalability of Spark Climate Job")
+plt.title("Scalability of Spark Climate Job (Large Dataset)")
 plt.grid()
 plt.show()
