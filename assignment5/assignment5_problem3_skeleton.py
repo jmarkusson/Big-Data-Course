8c2bc753a13a6e4277682f328427e2949d9a85e4
jmarkusson <jomarkusson@gmail.com>
2026-05-18 11:01:19 +0200

    fixed the skeleton for problem 3, TODO implement hyperLOGLOG

--- assignment5/assignment5_problem3_skeleton.py
+++ assignment5/assignment5_problem3_skeleton.py
@@ -9,8 +9,55 @@ import time
 
 def murmur3_32(key, seed):
     """Computes the 32-bit murmur3 hash"""
-    # copy from Problem 1
-    raise NotImplementedError()
+    c1 = 0xcc9e2d51
+    c2 = 0x1b873593
+    r1 = 15
+    r2 = 13
+    m = 5
+    n = 0xe6546b64
+
+    data = key.encode('utf-8')
+    length = len(data)
+    hash = seed
+
+    num_blocks = length // 4
+
+    for i in range(num_blocks):
+        k = int.from_bytes(data[i*4:(i+1)*4], byteorder='little')
+
+        k = (k * c1) & 0xffffffff # Appropriate mask
+        k = rol32(k, r1)
+        k = (k * c2) & 0xffffffff
+
+        hash ^= k
+        hash = rol32(hash, r2)
+        hash = ((hash * m) + n) & 0xffffffff
+
+    tail_index = num_blocks * 4
+    remaining_bytes = length % 4
+
+    k1 = int.from_bytes(data[tail_index:tail_index + remaining_bytes], byteorder='little')
+    if remaining_bytes > 0:
+        k1 = (k1 * c1) & 0xffffffff
+        k1 = rol32(k1, r1)
+        k1 = (k1 * c2) & 0xffffffff
+        hash ^= k1
+
+
+    # Exakt som står på wiki
+    hash ^= length
+
+    hash ^= (hash >> 16)
+    hash = (hash * 0x85ebca6b) & 0xffffffff
+    hash ^= (hash >> 13)
+    hash = (hash * 0xc2b2ae35) & 0xffffffff
+    hash ^= (hash >> 16)
+
+    return hash
+
+def rol32(x,k):
+    """Auxiliary function (left rotation for 32-bit words)"""
+    return ((x << k) | (x >> (32-k))) & 0xffffffff
 
 def auto_int(x):
     """Auxiliary function to help convert e.g. hex integers"""
@@ -20,8 +67,11 @@ def dlog2(n):
     return n.bit_length() - 1
 
 def rho(n):
-    # Copy from Problem 2
-    raise NotImplementedError()
+    """Given a 32-bit number n, return the 1-based position of the first
+    1-bit"""
+    if n == 0:
+        return 0
+    return 32 - n.bit_length() + 1
 
 def compute_jr(key,seed,log2m):
     """hash the string key with murmur3_32, using the given seed
@@ -36,8 +86,11 @@ def compute_jr(key,seed,log2m):
 
     Return a tuple (j,r) of integers
     """
-    # Copy from Problem 2
-    raise NotImplementedError()
+    h = murmur3_32(key,seed)
+    print(f'{h:08x}')
+    j = ~(0xffffffff << log2m) & h
+    r = rho(h)
+    return j, r
 
 def get_files(path):
     """
